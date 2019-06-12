99d043f4a65af14e4bdcdd08af4efbd94cee4892
Alando Appling <alandoappling@gmail.com>
2019-06-11 23:11:32 -0700

    Added neccessary components

--- butler/src/components/aboutus.js
+++ butler/src/components/aboutus.js
@@ -7,4 +7,4 @@ const AboutUs = props => (
     </div>
 )
 
-export default AboutUs
+export default AboutUs;
---
+++ butler/src/components/contactus.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ContactUs = props => {
+    <div>
+        <h2>Contact Us</h2>
+        <p>Secure your </p>
+    </div>
+}
